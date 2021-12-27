f4efc402a50d9bde33c5e487162bbbb012c675ba
rrich-kray <rrich.kray@gmail.com>
2021-12-26 23:15:40 -0500

    reorganized root folder

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# task-calendar
+# Task Calendar
+
+## The Repository
+
+This repository contains the HTML, CSS and Javascript code for the Task Calendar Project.
+
+## Task Calendar
+
+![Task Calendar Screenshot](./assets/images/screen1.png)
+
+Never lose track of an important meeting again! With Task Calendar, users can keep track of important events and better manage their time.
+
+## Usage
+
+Usage is simple. The current day is displayed in real-time at the top of the page, as shown in below. 
+
+![Length Prompt Screenshot](./assets/images/screen2.png)
+
+![Content Prompt Screenshot](./assets/images/screen6.png)
+
+
+![Content Prompt Screenshot](./assets/images/screen3.png)
+
+![Invalid input prompt screenshot](./assets/images/screen7.png)
+
+
+![Invalid input prompt screenshot](./assets/images/screen8.png)
+
+
+![Invalid input prompt screenshot](./assets/images/screen9.png)
+
+![Invalid input prompt screenshot](./assets/images/screen5.png)
+
+## Deployed Application
+
+the deployed application can be found by following the link below.
+
+https://rrich-kray.github.io/task-calendar/
+
+## Feedback
+
+Leave comments/suggestions in the Slack channel!
+
+## License
+
+Copyright &copy; Coding Quiz, Inc. All rights reserved.
+
+
---
+++ app.js
@@ -0,0 +1,72 @@
+var events = {
+        "8:00AM": [],
+        "9:00AM": [],
+        "10:00AM": [],
+        "11:00AM": [],
+        "12:00PM": [],
+        "1:00PM": [],
+        "2:00PM": [],
+        "3:00PM": [],
+        "4:00PM": [],
+        "5:00PM": [],
+        "6:00PM": [],
+        "7:00PM": [],
+        "8:00PM": [],
+}
+
+var getCurrentTime = setInterval(function(){
+    var currentDay = moment();
+    document.querySelector("#currentDay").innerHTML = currentDay.format('dddd MMMM Do YYYY, h:mm:ss a')
+}, 1000)
+
+
+var checkStorage = function(){
+    if (!window.localStorage.getItem('events')) {
+        window.localStorage.clear();
+        window.localStorage.setItem('events', JSON.stringify(events))
+    }
+}
+
+var colorHours = function(){
+    var now = moment();
+    var textBox = document.querySelector(".hour-body")
+    document.querySelectorAll('.hour').forEach(function(hour) {
+        if (now.valueOf() < hour.dataset.time) {textBox.classList.add("future")}
+        if (now.valueOf() === hour.dataset.time) {textBox.classList.add("present")}
+        if (now.valueOf() > hour.dataset.time) {textBox.classList.add("past")}
+    })
+}
+
+$('.hour-body').on('focus', function(){
+    $(this).animate({opacity: "-=.05"})
+})
+
+document.querySelector('.hour-body').addEventListener('keyup', function(event){
+    if (event.keyCode === 13) {
+        var eventObject = window.localStorage.getItem('events') // returns undefined
+        console.log(eventObject)
+        var tasks = eventObject[event.target.parentElement.dataset.time];
+        console.log(tasks) // returns undefined
+        tasks.push(event.target.textContent);
+        window.localStorage.setItem('events', JSON.stringify(timeObject));
+        textbox.textContent = "";
+    }
+})
+
+
+$('.hour').on('click', function(event){
+    var modal = document.querySelector('.modal');
+    if (event.target.classList.contains("display-events")) {
+        modal.style.display = "block";
+        var eventObject = JSON.parse(window.localStorage.getItem('scores'))
+        var tasks = eventObject[event.target.parentElement.dataset.time];
+        tasks.forEach(function(task){
+            var eventEl = document.createElement('div');
+            eventEl.textContent = task;
+            document.querySelector('.modal-content').appendChild(eventEl);
+        })
+    }
+})
+
+colorHours();
+checkStorage();
